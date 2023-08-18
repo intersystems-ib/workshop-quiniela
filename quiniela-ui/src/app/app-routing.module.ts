7fcbfa5994af1c84362b2e5af106c6cd2d7cfd23
isc-lperezra <lperezra@intersystems.com>
2023-08-18 09:45:03 +0200

    Interfaz updates

--- quiniela-ui/src/app/app-routing.module.ts
+++ quiniela-ui/src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './services/auth.guard';
-import { ImportComponent } from './import/import.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'import', component: ImportComponent, canActivate: [AuthGuard]},
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
--- quiniela-ui/src/app/data/data.component.ts
+++ quiniela-ui/src/app/data/data.component.ts
@@ -12,6 +12,8 @@ export class DataComponent {
   public dataCount: Array<any> = [];
   public totalDataRaw = 0;
   public totalDataPrepared = 0;
+  public totalOfModelsCreated = 0;
+  public totalOfModelsTrained = 0;
 
   subscription !: Subscription;
 
@@ -24,7 +26,9 @@ export class DataComponent {
     this.subscription = timer(0, 10000).pipe(switchMap(() => this.irisService.getStatus("GetDataCount"))).subscribe(res => {
       this.dataCount = JSON.parse(res.Result.replace("\\",""));
       this.totalDataRaw = this.dataCount[0].count;
-      this.totalDataPrepared = this.dataCount[1].count;       
+      this.totalDataPrepared = this.dataCount[1].count;
+      this.totalOfModelsCreated = this.dataCount[2].count;
+      this.totalOfModelsTrained = this.dataCount[3].count;       
     }
     );
         
